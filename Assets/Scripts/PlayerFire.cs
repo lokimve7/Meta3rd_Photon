3a9c1cf753c70b63f564e6e1f21e939785a38a8f
lokimve7 <lokimve7@gmail.com>
2024-08-28 13:55:39 +0900

    .

---
+++ Assets/Scripts/PlayerFire.cs
@@ -0,0 +1,30 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviourPun
+{
+    // 큐브 Prefab
+    public GameObject cubeFactory;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // 만약에 내 것이 아니라면
+        if (photonView.IsMine == false) return;
+        
+        // 1 번키 누르면
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            // 카메라의 앞방향으로 5만큼 떨어진 위치를 구하자.
+            Vector3 pos = Camera.main.transform.position + Camera.main.transform.forward * 5;
+            // 큐브공장에서 큐브를 생성, 위치, 회전
+            PhotonNetwork.Instantiate("Cube", pos, Quaternion.identity);
+        }        
+    }
+}
